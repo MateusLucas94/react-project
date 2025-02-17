ed12e76cb108d31dbefbcb61a8c0b82fa20bf0c4
Mateus Lucas <mateus.some.ms@gmail.com>
2025-02-17 15:11:04 -0300

    adicionado um botão com estilo no app para ver como ele reage ao click no react com um span.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import interstellarSrc from "./assets/interstellarSrc.jpg";
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("Esse filme é o melhor!");
+  }
+
   return (
     <div className="app">
       <Header />
@@ -29,6 +33,12 @@ function App() {
           <img src={interstellarSrc} alt="Poster 1 interstellar" />
           <img src="/interstellarPublic.jpg" alt="Poster 2 interstellar" />
         </div>
+        {/* Dá pra fazer o botão das duas formas: */}
+        <button onClick={handleClick}>Esse filme é bom?</button>
+        {/* Da segunda forma não precisa da função lá em cima, porém dependendo da função tem que ver qual compensa! Acho que a de baixo é melhor para quando é um botão bem simples, ai fica um codigo mais limpo. */}
+        {/* <button onClick={() => alert("Esse filme é o melhor!")}>
+          Esse filme é bom?
+        </button> */}
       </div>
       <Footer />
     </div>
